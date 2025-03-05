421b6eddc332bb21a39336b33cb93ad7fc4417e7
mehthishamkhan06 <mehthishamkhan06@gmail.com>
2025-03-06 00:03:17 +0400

    continue from aiming controls

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -4,6 +4,7 @@ public class BallController : MonoBehaviour
 {
     [SerializeField] private float force = 1f;
     [SerializeField] private InputManager inputManager;
+    [SerializeField] private Transform ballAnchor;
     private bool isBallLaunched;
     private Rigidbody ballRB;
     void Start()
@@ -14,12 +15,17 @@ public class BallController : MonoBehaviour
     // When the space key is pressed the
     // LaunchBall method will be called.
         inputManager.OnSpacePressed.AddListener(LaunchBall);
+        transform.parent = ballAnchor;
+        transform.localPosition = Vector3.zero;
+        ballRB.isKinematic = true;
     }
 
     private void LaunchBall()
     {
         if (isBallLaunched) return;
         isBallLaunched = true;
+        transform.parent = null;
+        ballRB.isKinematic = false;
         ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
     }
 }
